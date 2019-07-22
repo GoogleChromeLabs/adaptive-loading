ee535db589a26bcb901c445a9b780ca5db270190
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-07-22 22:13:30 +0300

    porting helper to react hook

--- cra-ua-aware-code-splitting/src/utils/hooks.js
+++ cra-ua-aware-code-splitting/src/utils/hooks.js
@@ -14,26 +14,36 @@
  * limitations under the License.
  */
 
+import { useState, useEffect } from 'react';
 import UAParser from 'ua-parser-js';
 
-export const getDeviceClass = () => {
+const useDeviceClass = () => {
+  const [deviceClass, setDeviceClass] = useState(null);
+
+  useEffect(() => {
     // Detect device model from UA
     const parser = new UAParser();
     const uastring = navigator.userAgent;
     parser.setUA(uastring);
     const device = parser.getDevice();
     const model = device.model;
-    console.log('[helpers] device mode => ', model);
+    console.log('[useDeviceClass] device model => ', model);
 
     // Match against devices you consider low-end
     const lowEnd = [
-        'Nexus 4',
-        'Nexus 5',
-        'Nexus 5X',
-        'Nexus 6',
-        'Redmi Note 6 Pro',
-        'ONE' // Alcatel 1X
-    ];
-    // Optional: map to device-year-class, Geekbench.
-    return lowEnd.indexOf(model) > 0 ? 'light' : 'heavy'
+      'Nexus 4',
+      'Nexus 5',
+      'Nexus 5X',
+      'Nexus 6',
+      'Redmi Note 6 Pro',
+      'ONE' // Alcatel 1X
+  ];
+
+  // Optional: map to device-year-class, Geekbench.
+  setDeviceClass(lowEnd.indexOf(model) > 0 ? 'light' : 'heavy');
+  },[]);
+
+  return deviceClass;
 };
+
+export { useDeviceClass };
