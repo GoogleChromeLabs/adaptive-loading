269033b03f63edd392489c51afd7549fdd490e03
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-07-28 00:55:24 +0300

    tested multiple deployment at a go

--- react-movie-network-aware-components/src/components/App/App.js
+++ react-movie-network-aware-components/src/components/App/App.js
@@ -7,7 +7,7 @@ import NotFound from "../elements/NotFound/NotFound";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/react-movie-network-aware-components">
       <Fragment>
         <Header />
         <Switch>
