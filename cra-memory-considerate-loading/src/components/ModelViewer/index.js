c175e8e837a7999412ad6ded6e7277f5be27d068
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-08-02 01:54:13 +0300

    updated memory considerate loading strategy

--- cra-memory-considerate-loading/src/components/ModelViewer/index.js
+++ cra-memory-considerate-loading/src/components/ModelViewer/index.js
@@ -22,32 +22,17 @@ const LazyModel3DViewer = lazy(() => import('./Model3DViewer'));
 const LazyModelImageViewer = lazy(() => import('./ModelImageViewer'));
 
 const ModelViewer = ({ src, fallbackSrc, memoryStatus }) => {
-  const { usedMemoryPercent } = memoryStatus;
+  const { overLoad } = memoryStatus;
 
-  let viewer = null;
-  switch(true) {
-    case usedMemoryPercent > 75:
-      viewer = (
-        <Suspense fallback={<Loading />}>
-          <LazyModelImageViewer src={fallbackSrc} />
-        </Suspense>
-      );
-      break;
-    case usedMemoryPercent > 0:
-      viewer = (
-        <Suspense fallback={<Loading />}>
-          <LazyModel3DViewer src={src} />
-        </Suspense>
-      );
-      break;
-    default:
-      viewer = (
-        <Suspense fallback={<Loading />}>
-          <LazyModel3DViewer src={src} />
-        </Suspense>
-      );
-      break;
-  }
+  const viewer = overLoad ? (
+    <Suspense fallback={<Loading />}>
+      <LazyModelImageViewer src={fallbackSrc} />
+    </Suspense>
+  ) : (
+    <Suspense fallback={<Loading />}>
+      <LazyModel3DViewer src={src} />
+    </Suspense>
+  );
 
   return (
     <Fragment>
