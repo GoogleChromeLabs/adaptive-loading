c712ff8223dbf706d092ed3f6074475d0d325124
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-08-01 22:31:12 +0300

    updated network react hooks structure for easy Unit Test

--- cra-network-aware-component/src/utils/hooks.js
+++ cra-network-aware-component/src/utils/hooks.js
@@ -17,23 +17,25 @@
 // inspired by https://github.com/rehooks/network-status
 import { useState, useEffect } from 'react';
 
-const navigatorConnection = navigator.connection;
-
-const getEffectiveConnectionType = () => {
-  return navigatorConnection ? navigatorConnection.effectiveType : null;
-};
-
 const useEffectiveConnectionType = () => {
+  const navigatorConnection = navigator.connection;
+  
+  const getEffectiveConnectionType = () => {
+    return navigatorConnection ? navigatorConnection.effectiveType : null;
+  };
+
   const [effectiveConnectionType, setEffectiveConnectionType] = useState(getEffectiveConnectionType());
 
   const updateECTStatus = () => {
     setEffectiveConnectionType(getEffectiveConnectionType());
   };
+  
   useEffect(() => {
     navigatorConnection && navigatorConnection.addEventListener('change', updateECTStatus);
     return () => {
       navigatorConnection && navigatorConnection.removeEventListener('change', updateECTStatus);
     };
+  // eslint-disable-next-line
   }, []);
 
   return effectiveConnectionType;
--- cra-network-aware-data-fetching/src/utils/hooks.js
+++ cra-network-aware-data-fetching/src/utils/hooks.js
@@ -16,23 +16,25 @@
 
 import { useState, useEffect } from 'react';
 
-const navigatorConnection = navigator.connection;
-
-const getEffectiveConnectionType = () => {
-  return navigatorConnection ? navigatorConnection.effectiveType : null;
-};
-
 const useEffectiveConnectionType = () => {
+  const navigatorConnection = navigator.connection;
+  
+  const getEffectiveConnectionType = () => {
+    return navigatorConnection ? navigatorConnection.effectiveType : null;
+  };
+
   const [effectiveConnectionType, setEffectiveConnectionType] = useState(getEffectiveConnectionType());
 
   const updateECTStatus = () => {
     setEffectiveConnectionType(getEffectiveConnectionType());
   };
+
   useEffect(() => {
     navigatorConnection && navigatorConnection.addEventListener('change', updateECTStatus);
     return () => {
       navigatorConnection && navigatorConnection.removeEventListener('change', updateECTStatus);
     };
+  // eslint-disable-next-line
   }, []);
 
   return effectiveConnectionType;
