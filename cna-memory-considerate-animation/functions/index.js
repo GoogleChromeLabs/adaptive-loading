b6d33bf23191cd0b6a133534f039317a8d734be3
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-10-14 01:28:56 +0300

    set configuration for firebase functions deployment of next.js app

---
+++ cna-memory-considerate-animation/functions/index.js
@@ -0,0 +1,12 @@
+
+const functions = require('firebase-functions');
+const next = require('next');
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev, conf: { distDir: 'next' } });
+const handle = app.getRequestHandler();
+
+exports.next = functions.https.onRequest((req, res) => {
+  console.log('file: ' + req.originalUrl); // log the page.js file that is being requested
+  return app.prepare().then(() => handle(req, res));
+});
