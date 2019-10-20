eebdb28b9342fde094049777a1a06721bbc7827a
Wing Leung <damianleung@gmail.com>
2019-10-20 16:53:14 +0200

    execute commands in predefined list of projects

--- build.sh
+++ build.sh
@@ -2,150 +2,33 @@
 multipleBuildsPath="../functions/builds/"
 individualBuildAllFiles="build/*"
 
-craBatteryConsiderateLoading="cra-battery-considerate-loading"
-craDeviceClassAwareLoading="cra-device-class-aware-loading"
-craMemoryConsiderateLoading="cra-memory-considerate-loading"
-craMemoryConsiderateLoadingSketchfab="cra-memory-considerate-loading-sketchfab"
-craNetworkAwareCodeSplitting="cra-network-aware-code-splitting"
-craNetworkAwareLoading="cra-network-aware-loading"
-craNetworkAwareOnlyIfCachedLoading="cra-network-aware-only-if-cached-loading"
-craNetworkAwareDataFetching="cra-network-aware-data-fetching"
-craUaAwareCodeSplitting="cra-ua-aware-code-splitting"
-reactMovieNetworkAwareLoading="react-movie-network-aware-loading"
-reactShrineNetworkAwareCodeSplitting="react-shrine-network-aware-code-splitting"
-nodeDprAwareLoading="node-dpr-aware-loading"
-nodeNetworkMemoryConsiderateLoading="node-network-memory-considerate-loading"
-nodeMemoryConsiderateLoading="node-memory-considerate-loading"
-nodeNetworkAwareLoading="node-network-aware-loading"
-reactTwitterSaveDataLoading="react-twitter-save-data-loading"
+for PROJECT in  cra-battery-considerate-loading \
+                cra-device-class-aware-loading \
+                cra-memory-considerate-loading \
+                cra-memory-considerate-loading-sketchfab  \
+                cra-network-aware-code-splitting \
+                cra-network-aware-loading \
+                cra-network-aware-only-if-cached-loading \
+                cra-network-aware-data-fetching \
+                cra-ua-aware-code-splitting \
+                react-movie-network-aware-loading \
+                react-shrine-network-aware-code-splitting \
+                node-dpr-aware-loading \
+                node-network-memory-considerate-loading \
+                node-memory-considerate-loading \
+                node-network-aware-loading \
+                react-twitter-save-data-loading
+do
+    echo "Start building ${PROJECT}..."
+    cd $PROJECT
 
-# cd cra-battery-considerate-loading
-cd $craBatteryConsiderateLoading
-rm -rf node_modules build
-npm install
-npm run build
-# mkdir -p ../functions/builds/cra-battery-considerate-loading
-mkdir -p "${multipleBuildsPath}${craBatteryConsiderateLoading}"
-# cp -r build/* ../functions/builds/cra-battery-considerate-loading
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${craBatteryConsiderateLoading}"
-cd ..
+    rm -rf node_modules build
+    npm install
+    npm run build
 
-cd $craDeviceClassAwareLoading
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${craDeviceClassAwareLoading}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${craDeviceClassAwareLoading}"
-cd ..
+    mkdir -p "${multipleBuildsPath}${PROJECT}"
+    cp -r $individualBuildAllFiles "${multipleBuildsPath}${PROJECT}"
 
-cd $craMemoryConsiderateLoading
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${craMemoryConsiderateLoading}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${craMemoryConsiderateLoading}"
-cd ..
-
-cd $craMemoryConsiderateLoadingSketchfab
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${craMemoryConsiderateLoadingSketchfab}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${craMemoryConsiderateLoadingSketchfab}"
-cd ..
-
-cd $craNetworkAwareCodeSplitting
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${craNetworkAwareCodeSplitting}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${craNetworkAwareCodeSplitting}"
-cd ..
-
-cd $craNetworkAwareLoading
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${craNetworkAwareLoading}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${craNetworkAwareLoading}"
-cd ..
-
-cd $craNetworkAwareOnlyIfCachedLoading
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${craNetworkAwareOnlyIfCachedLoading}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${craNetworkAwareOnlyIfCachedLoading}"
-cd ..
-
-cd $craNetworkAwareDataFetching
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${craNetworkAwareDataFetching}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${craNetworkAwareDataFetching}"
-cd ..
-
-cd $craUaAwareCodeSplitting
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${craUaAwareCodeSplitting}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${craUaAwareCodeSplitting}"
-cd ..
-
-cd $reactMovieNetworkAwareLoading
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${reactMovieNetworkAwareLoading}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${reactMovieNetworkAwareLoading}"
-cd ..
-
-cd $reactShrineNetworkAwareCodeSplitting
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${reactShrineNetworkAwareCodeSplitting}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${reactShrineNetworkAwareCodeSplitting}"
-cd ..
-
-cd $nodeDprAwareLoading
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${nodeDprAwareLoading}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${nodeDprAwareLoading}"
-cd ..
-
-cd $nodeNetworkMemoryConsiderateLoading
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${nodeNetworkMemoryConsiderateLoading}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${nodeNetworkMemoryConsiderateLoading}"
-cd ..
-
-cd $nodeMemoryConsiderateLoading
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${nodeMemoryConsiderateLoading}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${nodeMemoryConsiderateLoading}"
-cd ..
-
-cd $nodeNetworkAwareLoading
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${nodeNetworkAwareLoading}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${nodeNetworkAwareLoading}"
-cd ..
-
-cd $reactTwitterSaveDataLoading
-rm -rf node_modules build
-npm install
-npm run build
-mkdir -p "${multipleBuildsPath}${reactTwitterSaveDataLoading}"
-cp -r $individualBuildAllFiles "${multipleBuildsPath}${reactTwitterSaveDataLoading}"
-cd ..
+    cd ..
+    echo "Done building ${PROJECT}."
+done
--- clean.sh
+++ clean.sh
@@ -2,87 +2,29 @@
 multipleBuildsPath="../functions/builds/"
 individualBuildAllFiles="build/*"
 
-craBatteryConsiderateLoading="cra-battery-considerate-loading"
-craDeviceClassAwareLoading="cra-device-class-aware-loading"
-craMemoryConsiderateLoading="cra-memory-considerate-loading"
-craMemoryConsiderateLoadingSketchfab="cra-memory-considerate-loading-sketchfab"
-craNetworkAwareCodeSplitting="cra-network-aware-code-splitting"
-craNetworkAwareLoading="cra-network-aware-loading"
-craNetworkAwareOnlyIfCachedLoading="cra-network-aware-only-if-cached-loading"
-craNetworkAwareDataFetching="cra-network-aware-data-fetching"
-craUaAwareCodeSplitting="cra-ua-aware-code-splitting"
-reactMovieNetworkAwareLoading="react-movie-network-aware-loading"
-reactShrineNetworkAwareCodeSplitting="react-shrine-network-aware-code-splitting"
-nodeDprAwareLoading="node-dpr-aware-loading"
-nodeNetworkMemoryConsiderateLoading="node-network-memory-considerate-loading"
-nodeMemoryConsiderateLoading="node-memory-considerate-loading"
-nodeNetworkAwareLoading="node-network-aware-loading"
-reactTwitterSaveDataLoading="react-twitter-save-data-loading"
+for PROJECT in  functions \
+                cra-battery-considerate-loading \
+                cra-device-class-aware-loading \
+                cra-memory-considerate-loading \
+                cra-memory-considerate-loading-sketchfab  \
+                cra-network-aware-code-splitting \
+                cra-network-aware-loading \
+                cra-network-aware-only-if-cached-loading \
+                cra-network-aware-data-fetching \
+                cra-ua-aware-code-splitting \
+                react-movie-network-aware-loading \
+                react-shrine-network-aware-code-splitting \
+                node-dpr-aware-loading \
+                node-network-memory-considerate-loading \
+                node-memory-considerate-loading \
+                node-network-aware-loading \
+                react-twitter-save-data-loading
+do
+    echo "Start cleanup of ${PROJECT}..."
+    cd $PROJECT
 
-cd functions
-rm -rf node_modules builds
-cd ..
+    rm -rf node_modules build builds
 
-cd $craBatteryConsiderateLoading
-rm -rf node_modules build
-cd ..
-
-cd $craDeviceClassAwareLoading
-rm -rf node_modules build
-cd ..
-
-cd $craMemoryConsiderateLoading
-rm -rf node_modules build
-cd ..
-
-cd $craMemoryConsiderateLoadingSketchfab
-rm -rf node_modules build
-cd ..
-
-cd $craNetworkAwareCodeSplitting
-rm -rf node_modules build
-cd ..
-
-cd $craNetworkAwareLoading
-rm -rf node_modules build
-cd ..
-
-cd $craNetworkAwareOnlyIfCachedLoading
-rm -rf node_modules build
-cd ..
-
-cd $craNetworkAwareDataFetching
-rm -rf node_modules build
-cd ..
-
-cd $craUaAwareCodeSplitting
-rm -rf node_modules build
-cd ..
-
-cd $reactMovieNetworkAwareLoading
-rm -rf node_modules build
-cd ..
-
-cd $reactShrineNetworkAwareCodeSplitting
-rm -rf node_modules build
-cd ..
-
-cd $nodeDprAwareLoading
-rm -rf node_modules build
-cd ..
-
-cd $nodeNetworkMemoryConsiderateLoading
-rm -rf node_modules build
-cd ..
-
-cd $nodeMemoryConsiderateLoading
-rm -rf node_modules build
-cd ..
-
-cd $nodeNetworkAwareLoading
-rm -rf node_modules build
-cd ..
-
-cd $reactTwitterSaveDataLoading
-rm -rf node_modules build
-cd ..
+    cd ..
+    echo "Cleanup of ${PROJECT} done."
+done
