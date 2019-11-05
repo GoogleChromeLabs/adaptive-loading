edcfcc4358351eb94d314c4d0509abc5d7f8af3e
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-11-05 22:21:03 +0300

    set configuration for project naming

--- react-youtube/src/index.js
+++ react-youtube/src/index.js
@@ -1,17 +1,34 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import 'semantic-ui-css/semantic.min.css';
 import { Provider } from 'react-redux';
-import {BrowserRouter} from 'react-router-dom';
-import {configureStore} from './store/configureStore';
+import { configureStore } from './store/configureStore';
 
 const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename='/react-youtube-adaptive-loading'>
       <App/>
     </BrowserRouter>
   </Provider>, document.getElementById('root'));
